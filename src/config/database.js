7aa0f62e61473c8b2792026a0023640a0cf2bd13
Alini Canedo <alini.canedo@gmail.com>
2021-09-11 22:03:23 -0300

    Criando a estrutura do projeto e criando uma migrate para testar conexão com o banco

---
+++ src/config/database.js
@@ -0,0 +1,14 @@
+module.exports = {
+  host: 'localhost',
+  username: 'postgres',
+  password: '123456',
+  database: 'nodeauth',
+  dialect: 'postgres',
+  operatorsAliases: false,
+  logging: false,
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true,
+  },
+};
